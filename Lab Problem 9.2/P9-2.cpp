3bec99fa4c67797e1a2f215b1268db952f9bb238
Wesley Casselman <w-casselman@onu.edu>
2023-10-26 09:11:46 -0400

    Completed isSortedIncreasing

--- Lab Problem 9.2/P9-2.cpp
+++ Lab Problem 9.2/P9-2.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+bool isSortedIncreasing(int array[], int size);
+
 int main(void)
 {
 	const int SIZE = 6;
@@ -17,12 +19,19 @@ int main(void)
 		cin >> arr[i];
 	}
 
-	for (int i = 0; i < SIZE; i++)
-	{
-		cout << arr[i] << " ";
-	}
-
-	cout << endl;
+	isSortedIncreasing(arr, SIZE) ? cout << "The data are increasing." << endl : cout << "The data are not increasing." << endl;
 
 	return 0;
+}
+
+bool isSortedIncreasing(int array[], int size)
+{
+	for (int i = 1; i < size; i++)
+	{
+		if (array[i - 1] > array[i])
+		{
+			return false;
+		}
+	}
+	return true;
 }
