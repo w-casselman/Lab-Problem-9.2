ecdbabf0e9cab0b596173c080a4d3336a3a2916e
Wesley Casselman <w-casselman@onu.edu>
2023-10-26 09:03:10 -0400

    Array can be entered

--- Lab Problem 9.2/P9-2.cpp
+++ Lab Problem 9.2/P9-2.cpp
@@ -1,3 +1,28 @@
 // P9-2
 // Wesley Casselman
 // October 26th of 2023
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	const int SIZE = 6;
+
+	int arr[SIZE];
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << "Enter integer #" << i + 1 << ": ";
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+
+	return 0;
+}
